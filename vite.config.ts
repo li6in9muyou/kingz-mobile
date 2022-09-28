9ca17f8f2c086e4fa7204125b4df4a8c85196e22
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-09-28 17:13:50 +0800

    chore: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
