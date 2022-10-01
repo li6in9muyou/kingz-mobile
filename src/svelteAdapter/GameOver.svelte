38d446fef11bc9fcca1b0afc75f7ba2d2686c4b6
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-01 21:01:17 +0800

    style: GameOver.svelte

--- src/svelteAdapter/GameOver.svelte
+++ src/svelteAdapter/GameOver.svelte
@@ -3,7 +3,42 @@
   const dispatch = createEventDispatcher();
 </script>
 
-<h1>Game Over</h1>
-<p>
-  <button on:click={() => dispatch("backToWelcomePage")}>Restart</button>
-</p>
+<main>
+  <section on:click={() => dispatch("backToWelcomePage")}>
+    <h1>Game Over</h1>
+    <div>click to go back</div>
+  </section>
+</main>
+
+<style>
+  * {
+    color: whitesmoke;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: black;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  section > * {
+    text-align: center;
+  }
+
+  section > div {
+    margin-top: 2rem;
+  }
+
+  h1 {
+    animation: float 2s ease-in-out infinite;
+    font-size: 4rem;
+    font-family: sans-serif;
+    font-weight: bold;
+    background-image: linear-gradient(60deg, #e21143, #ffb03a);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
