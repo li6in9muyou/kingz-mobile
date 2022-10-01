d3eea0d4701ffdcac6a2eb2cefd85bb97d019545
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-01 20:58:44 +0800

    style: StartNewGame.svelte

--- src/svelteAdapter/StartNewGame.svelte
+++ src/svelteAdapter/StartNewGame.svelte
@@ -4,12 +4,48 @@
   export let use_case: LocalIdentity;
 </script>
 
-<h1>To start new Game, enter a nickname</h1>
-<input id="nickname" type="text" />
-<button
-  on:click={() => {
-    use_case.on_register(document.getElementById("nickname").value);
-  }}
->
-  submit
-</button>
+<main>
+  <h1>To start new Game, enter a nickname</h1>
+  <input id="nickname" type="text" />
+  <button
+    on:click={() => {
+      use_case.on_register(document.getElementById("nickname").value);
+    }}
+  >
+    submit
+  </button>
+</main>
+
+<style>
+  * {
+    color: whitesmoke;
+    background: black;
+  }
+
+  h1 {
+    animation: float 2s ease-in-out infinite;
+  }
+
+  input {
+    font-size: 3rem;
+    background: whitesmoke;
+    color: black;
+  }
+
+  button {
+    border-radius: 1rem;
+    background: lightcoral;
+    padding: 0.5rem 2rem;
+    font-size: 1.6rem;
+  }
+
+  main {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 2rem;
+    gap: 2rem;
+  }
+</style>
