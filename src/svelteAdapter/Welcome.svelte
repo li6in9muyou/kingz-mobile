66016accb0308e7839cf81d093d9885f1ded77fd
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-01 20:49:50 +0800

    style: add some style

--- src/svelteAdapter/Welcome.svelte
+++ src/svelteAdapter/Welcome.svelte
@@ -3,7 +3,54 @@
   const dispatch = createEventDispatcher();
 </script>
 
-<h1>Welcome to Kingz</h1>
-<p>
-  <button on:click={() => dispatch("enterGame")}>Enter game</button>
-</p>
+<main>
+  <section on:click={() => dispatch("enterGame")}>
+    <h1>Welcome to Kingz</h1>
+    <div>click to enter</div>
+  </section>
+</main>
+
+<style>
+  * {
+    color: whitesmoke;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: black;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  section > * {
+    text-align: center;
+  }
+
+  section > div {
+    margin-top: 2rem;
+  }
+
+  h1 {
+    animation: float 2s ease-in-out infinite;
+    font-size: 4rem;
+    font-family: sans-serif;
+    font-weight: bold;
+    background-image: linear-gradient(60deg, #e21143, #ffb03a);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  @keyframes float {
+    0% {
+      transform: translatey(0px);
+    }
+    50% {
+      transform: translatey(-20px);
+    }
+    100% {
+      transform: translatey(0px);
+    }
+  }
+</style>
